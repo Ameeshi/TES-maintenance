214f602e08f61730e9692065452d67d9358c6614
Daniel Graf <grafmark59@gmail.com>
2017-06-30 16:09:57 +0900

    Added home controller

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise'
 
+gem 'bootstrap' #, '~> 4.0.0.alpha3'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :classrooms
   resources :schools
   devise_for :users
+
+  root to: "home#index"
+  get 'home', to: redirect('/')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

c5e9753f386b55463315de6c081ee3d73c785960
Daniel Graf <grafmark59@gmail.com>
2017-07-04 11:53:19 +0900

    Created partials, fixed alerts to fade, made small tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  
+#  layout :layout
+#  
+#  private
+#
+#  def layout
+#    # only turn it off for login pages:
+#    is_a?(Devise::SessionsController) ? false : "application"
+#    # or turn layout off for every devise controller:
+#    # devise_controller? && "application"
+#  end
 end

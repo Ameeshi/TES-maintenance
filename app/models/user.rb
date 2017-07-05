15471d163d5f4d90d564540c6a3be1db791d180e
Daniel Graf <grafmark59@gmail.com>
2017-07-05 14:06:26 +0900

    School show and a few small things

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  ############ Role methods ############
+  # Shouldn't be necessary because no one should have more than
+  # one role, but it's a safeguard
+  def highestRole
+    if self.has_role? :admin
+      return 'Admin'
+    elsif self.has_role? :specialist
+      return 'Specialist'
+    elsif self.has_role? :manager
+      return 'Manager'
+    elsif self.has_role? :principal
+      return 'Principal'
+    elsif self.has_role? :teacher
+      return 'Teacher'
+    else
+      # Should never be none when used
+      return 'None'
+    end
+  end
+  
+  
 end

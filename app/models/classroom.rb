a462a1c4b1b720537529116d27678f97e1494159
Daniel Graf <grafmark59@gmail.com>
2017-07-11 22:30:29 +0900

    fixed classroom

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -21,15 +21,18 @@ class Classroom < ApplicationRecord
   
   # Validations
   validates_presence_of :teacher_id, :school_id, :content_area, :grade
+  
   validates_inclusion_of :grade, in: GRADES_LIST.map{|value| value}, message: "is not an option"
   validates_inclusion_of :content_area, in: CONTENT_AREAS_LIST.map{|value| value}, message: "is not an option"
 #  validate :school_is_not_a_duplicate, on: :create
+  validate :teacher_is_teacher, on: :create
 
 #  def already_exists?
 #    School.where(name: self.name, state: self.state).size == 1
 #  end
 
   # Callbacks
+  
   before_destroy :is_destroyable?
   after_rollback :make_inactive_if_trying_to_destroy
 
@@ -48,6 +51,11 @@ class Classroom < ApplicationRecord
     @destroyable = nil
   end
 
+  def teacher_is_teacher
+    return true if self.teacher.has_role? :teacher
+    errors.add(self.teacher.name, "is not a teacher.")
+  end
+  
 #  def classroom_is_not_a_duplicate
 #    return true if self.teacher_id.nil? || self.state.nil?
 #    if self.already_exists?

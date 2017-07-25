970fcf8194ab377423079053e8d1440d47063ae3
Daniel Graf <grafmark59@gmail.com>
2017-07-25 16:22:19 +0900

    made delete option for classroom, working on callback

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module TeacherEvaluationSystem
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     
+    # This fixes the form issue when submitting incorrect info
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
       html_tag
     }
